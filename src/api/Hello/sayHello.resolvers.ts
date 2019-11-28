af4942088910a80d9805088e13ce5c890ed23f89
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-11-28 11:01:39 +0900

    GraphQL Server 설정중

---
+++ src/api/Hello/sayHello.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        sayHello: () => "Hey hello there"
+    }
+}
+
+export default resolvers;
---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import cors from "cors"
+import { GraphQLServer } from "graphql-yoga"
+import logger from "morgan"
+import helmet from "helmet"
+import schema from "./schema"
+
+class App {
+    public app: GraphQLServer;
+    constructor() {
+        this.app = new GraphQLServer({
+            schema: schema
+        });
+        this.middleware();
+    }
+    private middleware = () => {
+        this.app.express.use(cors());
+        this.app.express.use(logger("dev"));
+        this.app.express.use(helmet());
+    };
+}
+
+export default new App().app;
