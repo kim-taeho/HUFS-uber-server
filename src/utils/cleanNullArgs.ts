a72789c69f75df573b3990d393d5029bd5dded1f
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-12-03 19:03:53 +0900

    ReportMovement 완료

---
+++ src/utils/cleanNullArgs.ts
@@ -0,0 +1,12 @@
+const cleanNullArgs = (args: object): object => {
+    const notNull: any = {};
+
+    Object.keys(args).forEach(key => {
+        if (args[key] !== null) {
+            notNull[key] = args[key];
+        }
+    });
+    return notNull;
+}
+
+export default cleanNullArgs;
