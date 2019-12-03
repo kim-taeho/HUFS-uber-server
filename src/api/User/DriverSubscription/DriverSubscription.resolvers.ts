e3043c44d5d4ebf1134163e19bc28510ded6b04c
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-12-04 02:46:49 +0900

    Subscription Socket 추가, withFilter 추가

--- src/api/User/DriverSubscription/DriverSubscription.resolvers.ts
+++ src/api/User/DriverSubscription/DriverSubscription.resolvers.ts
@@ -1,9 +1,29 @@
+import { withFilter } from "graphql-yoga";
+import User from "../../../entities/User";
+
 const resolvers = {
     Subscription: {
         DriverSubscription: {
-            subscribe: (_, __, { pubSub }) => {
-                return pubSub.asynciterator("driverUpdate");
-            }
+            subscribe: withFilter(
+                (_, __, { pubSub }) => pubSub.asynciterator("driverUpdate"),
+                (payload, _, { context }) => {
+                    const user: User = context.currentUser;
+                    const {
+                        DriverSubscription: {
+                            lastLat: driverLastLat,
+                            lastLng: driverLastLng
+                        }
+                    } = payload;
+                    const { lastLat: userLastLat, lastLng: userLastLng } = user;
+                    return (
+                        driverLastLat >= userLastLat - 0.05 &&
+                        driverLastLat <= userLastLat + 0.05 &&
+                        driverLastLng >= userLastLng - 0.05 &&
+                        driverLastLng <= userLastLng + 0.05
+                    );
+                }
+            )
+
         }
     }
 }
--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -12,7 +12,8 @@ const resolvers: Resolvers = {
                 const notNull = cleanNullArgs(args);
                 try {
                     await User.update({ id: user.id }, { ...notNull });
-                    pubSub.publish("driverUpdate", { DriverSubscription: user });
+                    const updatedUser = await User.findOne({ id: user.id });
+                    pubSub.publish("driverUpdate", { DriverSubscription: updatedUser });
                     return {
                         ok: true,
                         error: null
