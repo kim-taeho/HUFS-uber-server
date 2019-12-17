de50deb4122164fa6f9d32a546274e7a6f61e0e3
kimtaeho <xxxxoghxxx.thkim@gmail.com>
2019-12-17 16:10:46 +0900

    fix Bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+// dotenv.config({ path: "../.env" });
+dotenv.config();
 
 import { Options } from "graphql-yoga"
 import { createConnection } from "typeorm"
